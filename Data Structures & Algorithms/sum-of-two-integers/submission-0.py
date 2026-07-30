9744ac52c7d82997d9bffcb5a7034a468ceba6b5
Mingyao Li <106688909+mingyao1@users.noreply.github.com>
2026-07-30 16:12:09 -0500

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+
+        while b != 0:
+            carry = a & b
+            a = (a ^ b) & mask
+            b = (carry << 1) & mask
+
+        return a if a <= 0x7FFFFFFF else ~(a ^ mask)
